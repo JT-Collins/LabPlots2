79e66397ca77e019e422ef4324525221ed47a51e
JT-Collins <63314510+JT-Collins@users.noreply.github.com>
2023-05-11 10:24:39 -0400

    new function

---
+++ R/cerillo_excel.R
@@ -0,0 +1,26 @@
+#' @param filename The excel file that contains your OD600 readings in the 'dat' sheet and the variable data on the 'map' sheet
+#'
+#' @export
+
+cerillo_excel <- function(filename) {
+
+  well_coordinates <- expand.grid(row = LETTERS[1:8], col = 1:12)
+  well_coordinates <-  well_coordinates[
+    with(well_coordinates, order(row, col)),
+  ]
+  well_coordinates$well <- paste0(well_coordinates$row, well_coordinates$col)
+
+  # Create a workbook
+  wb <- openxlsx::createWorkbook()
+
+  # Add the "dat" sheet and write the well coordinates
+  openxlsx::addWorksheet(wb, "dat")
+  openxlsx::writeData(wb, "dat", x = c("Well", well_coordinates$well), startCol = 1, startRow = 1)
+
+  # Add the "map" sheet
+  openxlsx::addWorksheet(wb, "map")
+
+  # Save the workbook to a file
+  openxlsx::saveWorkbook(wb, filename, overwrite = TRUE)
+
+}
--- R/load_cerillo.R
+++ R/load_cerillo.R
@@ -5,34 +5,34 @@
 #' @export
 load_cerillo <- function(filename, Time_int = 10) {
 
-factor_names <- c("Plate", "Biol", "Biological", "Tech", "Technical", "Rep", "Replicate")
+  factor_names <- c("Plate", "Biol", "Biological", "Tech", "Technical", "Rep", "Replicate")
 
-# Read in Cerillo data and map file ---------------------------------------
+  # Read in Cerillo data and map file ---------------------------------------
 
-dat <- readxl::read_excel(filename,
-                  sheet = "dat")
+  dat <- readxl::read_excel(filename,
+                            sheet = "dat")
 
-map <- readxl::read_excel(filename,
-                  sheet = "map")
-
-
-
-# Get into correct structure and join -------------------------------------
-
-max_time <- (nrow(dat) -1) * Time_int # the total time of the experiment - the -1 accounts for the zero timepoint
-
-df <-
-  data.table::transpose(dat) |>
-  dplyr::mutate(Well = colnames(dat), .before = V1) |>
-  dplyr::left_join(map) |>
-  janitor::clean_names(case = "upper_camel") |>
-  mutate(across(any_of(factor_names), as.factor)) |> # convert plate numbers etc to factors
-  #na.omit() |>
-  tidyr::pivot_longer(cols = where(is.double),
-               names_to = "Time",
-               values_to = "OD600") |>
-  dplyr::mutate(Time = rep(seq(0,max_time, by = Time_int), times = 96))
+  map <- readxl::read_excel(filename,
+                            sheet = "map")
 
 
-df
+
+  # Get into correct structure and join -------------------------------------
+
+  max_time <- (nrow(dat) -1) * Time_int # the total time of the experiment - the -1 accounts for the zero timepoint
+
+  df <-
+    data.table::transpose(dat) |>
+    dplyr::mutate(Well = colnames(dat), .before = V1) |>
+    dplyr::left_join(map) |>
+    janitor::clean_names(case = "upper_camel") |>
+    mutate(across(any_of(factor_names), as.factor)) |> # convert plate numbers etc to factors
+    #na.omit() |>
+    tidyr::pivot_longer(cols = where(is.double),
+                        names_to = "Time",
+                        values_to = "OD600") |>
+    dplyr::mutate(Time = rep(seq(0,max_time, by = Time_int), times = 96))
+
+
+  df
 }
