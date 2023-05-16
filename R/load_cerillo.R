cccbd1c66a125002fc9bd2abd23d255c0184d4ea
JT-Collins <63314510+JT-Collins@users.noreply.github.com>
2023-05-16 14:47:52 -0400

    remove empty columns and convert all to char

--- R/load_cerillo.R
+++ R/load_cerillo.R
@@ -5,7 +5,7 @@
 #' @export
 load_cerillo <- function(filename, Time_int = 10) {
 
-  factor_names <- c("Plate", "Biol", "Biological", "Tech_rep", "Technical", "Biol_rep", "Replicate")
+  #factor_names <- c("Plate", "Biol", "Biological", "Tech_rep", "Technical", "BiolRep", "Replicate")
 
   # Read in Cerillo data and map file ---------------------------------------
 
@@ -15,20 +15,22 @@ load_cerillo <- function(filename, Time_int = 10) {
                             range = cellranger::cell_cols("F:CW"))
 
   map <- readxl::read_excel(filename,
-                            sheet = "map")
+                            sheet = "map") |>
+    janitor::remove_empty(which = "cols") |> # Drop columns with nothing in
+    dplyr::mutate(dplyr::across(tidyselect::everything(), as.character))
 
 
 
   # Get into correct structure and join -------------------------------------
 
-  max_time <- (nrow(dat)) * Time_int # the total time of the experiment - the -1 accounts for the zero timepoint
+  max_time <- (nrow(dat)) * Time_int-1 # the total time of the experiment - the -1 accounts for the zero timepoint
 
   df <-
     data.table::transpose(dat) |>
     dplyr::mutate(Well = colnames(dat), .before = V1) |>
     dplyr::left_join(map) |>
     janitor::clean_names(case = "upper_camel") |>
-    mutate(across(any_of(factor_names), as.factor)) |> # convert plate numbers etc to factors
+    #mutate(across(any_of(factor_names), as.factor)) |> # convert plate numbers etc to factors
     #na.omit() |>
     tidyr::pivot_longer(cols = where(is.double),
                         names_to = "Time",
