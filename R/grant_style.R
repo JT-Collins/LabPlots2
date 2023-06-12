db5b0e4f7a47b7cdec49dc2c9b99fd2abf953d03
JT-Collins <63314510+JT-Collins@users.noreply.github.com>
2023-06-12 16:52:46 -0400

    margin tweaks

--- R/grant_style.R
+++ R/grant_style.R
@@ -2,7 +2,7 @@
 grant_style <- function(colPal = c("uofl_col", "alt_col")) {
 
   # Check if required packages are installed and load the packages
-  required_packages <- c("hrbrthemes", "ggplot2", "grid")
+  required_packages <- c("hrbrthemes", "ggplot2", "grid", "ggtext")
   missing_packages <- required_packages[!sapply(required_packages, requireNamespace, quietly = TRUE)]
 
   if (length(missing_packages) > 0) {
@@ -14,7 +14,14 @@ grant_style <- function(colPal = c("uofl_col", "alt_col")) {
   colPal <- match.arg(colPal)
 
   # Set color palette
-  uofl_col <- c("#8B9DA1", "#AD0000", "#004E74", "#FEBE10" ,"#00A89D","#7A6C53",  "#AAB43A", "#D9C982")
+  uofl_col <- c("#ad0000",
+                              "#004e74",
+                              "#8b9da1",
+                              "#00a89d",
+                              "#ffba00",
+                              "#544009",
+                              "#698900",
+                              "#ddcb2e")
 
   alt_col <- c("#41afaa", "#466eb4", "#00a0e1", "#e6a532", "#d7642c", "#af4b91")
 
@@ -44,18 +51,25 @@ grant_style <- function(colPal = c("uofl_col", "alt_col")) {
       axis_col = "#5A5A5A",
       ticks = FALSE,
       base_size = 8,
-      plot_margin = ggplot2::margin(10, 5, 5, 10),
+      #plot_margin = ggplot2::margin(10, 5, 5, 10),
+      plot_margin = ggplot2::margin(2, 2, 2, 2),
       #panel_spacing = grid::unit(1, "lines"),
       plot_title_size = 9,
       subtitle_size = 8,
-      subtitle_margin = 10,
+      subtitle_margin = 5,
       axis_title_size = 9,
       strip_text_size = 8
           ) + theme(text = element_text(colour = mid_text),
-                    plot.title = element_text(colour = dark_text),
+                    plot.title = ggtext::element_markdown(color = dark_text, lineheight = 1.1),
+                    plot.subtitle = ggtext::element_markdown(lineheight = 1.1),
+                    #plot.title = element_text(colour = dark_text),
+                    plot.title.position = "plot",
                     axis.text.y = element_text(colour = light_text),
                     axis.line=element_line(linewidth=0.3),
                     axis.text.x = element_text(colour = light_text, margin = margin(t = 3, b = 3)))
   )
+  message(paste("ggtext is used to call plot.title and plot.subtitle enabling simple Markdown and HTML",
+                "rendering for ggplot2. See https://wilkelab.org/ggtext/"))
 
+  message(paste("If saving as a pdf file use 'device = cairo_pdf' to ensure correct font use"))
 }
--- R/lab_style.R
+++ R/lab_style.R
@@ -2,7 +2,7 @@
 lab_style <- function() {
 
   # Check if required packages are installed and load the packages
-  required_packages <- c("hrbrthemes", "ggplot2", "grid")
+  required_packages <- c("hrbrthemes", "ggplot2", "grid", "ggtext")
   missing_packages <- required_packages[!sapply(required_packages, requireNamespace, quietly = TRUE)]
 
   if (length(missing_packages) > 0) {
@@ -49,10 +49,17 @@ lab_style <- function() {
     ) + theme(text = ggplot2::element_text(colour = mid_text),
               axis.text.x = ggplot2::element_text(vjust = -1,colour = light_text),
               axis.text.y = ggplot2::element_text(margin = ggplot2::margin(r = 1), colour = light_text),
-              plot.title = ggplot2::element_text(color = dark_text),
+              plot.title = ggtext::element_markdown(color = dark_text, lineheight = 1.1),
+              plot.subtitle = ggtext::element_markdown(lineheight = 1.1),
+              #plot.title = ggplot2::element_text(color = dark_text),
               plot.title.position = "plot",
               axis.line = ggplot2::element_line(colour = light_text, linewidth = 0.5),
               axis.ticks.length = ggplot2::unit(3, "pt"))
   )
+
+message(paste("ggtext is used to call plot.title and plot.subtitle enabling simple Markdown and HTML",
+      "rendering for ggplot2. See https://wilkelab.org/ggtext/"))
+
+message(paste("If saving as a pdf file use 'device = cairo_pdf' to ensure correct font use"))
 
 }
