961224444109a906699908a0369e735b5e4684da
JT-Collins <63314510+JT-Collins@users.noreply.github.com>
2023-06-14 14:33:27 -0400

    Size tweaks and fig caption

--- R/grant_style.R
+++ R/grant_style.R
@@ -63,33 +63,57 @@ grant_style <- function(colPal = c("uofl_col", "alt_col")) {
       ticks = FALSE,
       base_size = 8,
       plot_margin = ggplot2::margin(2, 2, 2, 2),
-      plot_title_size = 9,
-      subtitle_size = 8,
+      plot_title_size = 10,
+      subtitle_size = 9,
       subtitle_margin = 4,
       axis_title_size = 9,
-      strip_text_size = 8
+      strip_text_size = 9,
+      caption_size = 8,
+      caption_face = "plain",
+      caption_margin = 4
           ) + theme(text = element_text(colour = mid_text),
                     plot.title = ggtext::element_markdown(color = dark_text, lineheight = 1.1,
                                                           margin = margin(b = 4.8),),
-                    plot.subtitle = ggtext::element_markdown(lineheight = 1.1,
+                    plot.subtitle = ggtext::element_textbox_simple(lineheight = 1.1,
                                                              margin = margin(b = 3.6)),
+                    plot.caption = ggtext::element_textbox_simple(lineheight = 1.1, colour = 	"#010B13"),
+                    plot.caption.position = "plot",
                     plot.title.position = "plot",
-                    axis.title.x = element_text(margin = margin(t = 4)),
+                    axis.title.x = element_text(margin = margin(t = 3, b = 3)),
                     axis.title.x.top = element_text(margin = margin(b = 4)),
                     axis.title.y = element_text(angle = 90, margin = margin(r = 4)),
                     axis.text.y = element_text(colour = light_text, margin = margin(r = 1.6)),
-                    #axis.line = element_line(linewidth=0.3),
                     axis.text.x = element_text(colour = light_text, margin = margin(t = 1.6)),
                     axis.text.x.top   = element_text(margin = margin(b = 1.6)),
                     axis.text.y.right = element_text(margin = margin(l = 1.6)),
                     axis.ticks.length = unit(2, "pt"),
                     panel.grid.major = element_line(linetype='dashed'),
                     panel.grid.minor = element_blank(),
-                    panel.spacing = grid::unit(4, "pt")
+                    panel.spacing = grid::unit(6, "pt"),
+                    legend.spacing        = unit(4, "pt"),
+                    legend.margin         = margin(0, 0, 0, 0, "cm"),
+                    legend.key.size       = grid::unit(0.5, "lines"),
+                    legend.box.margin     = margin(0, 0, 0, 0, "cm"),
+                    legend.box.background = element_blank(),
+                    legend.box.spacing    = unit(4, "pt")
 
   ))
 
-  update_geom_defaults("boxplot",   list(fill = "snow", linewidth = 0.2, colour = light_text))
+  update_geom_defaults("boxplot",
+                       list(
+                         fill = "gray98",
+                         linewidth = 0.2,
+                         colour = light_text
+                       ))
+  update_geom_defaults("point",
+                       list(
+                         shape = 21,
+                         fill = "gray90",
+                         color = dark_text,
+                         size = 1.8,
+                         alpha = 1,
+                         stroke = 0.2
+                       ))
 
   message(paste("ggtext is used to call plot.title and plot.subtitle enabling simple Markdown and HTML",
                 "rendering for ggplot2. See https://wilkelab.org/ggtext/"))
